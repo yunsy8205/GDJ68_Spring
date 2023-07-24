8ff49c21d63b5f4fb8d41652f950cff6e2324a25
yunsy8205 <yunsy8205@naver.com>
2023-07-24 11:56:07 +0900

    Study9

---
+++ src/main/java/com/iu/main/member/MemberDAO.java
@@ -0,0 +1,19 @@
+package com.iu.main.member;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+	
+	@Autowired // 객체 주입
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.iu.main.member.MemberDAO.";
+	
+	public MemberDTO getLogin(MemberDTO memberDTO) throws Exception{
+		return sqlSession.selectOne(NAMESPACE+"getLogin", memberDTO);
+	}
+	
+
+}
---
+++ src/main/java/com/iu/main/member/MemberService.java
@@ -0,0 +1,15 @@
+package com.iu.main.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	public MemberDTO getLogin(MemberDTO memberDTO) throws Exception{
+		return memberDAO.getLogin(memberDTO);
+	}
+}
